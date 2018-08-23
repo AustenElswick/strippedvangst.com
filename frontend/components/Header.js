074bde3c0a320c088ae9ab344b133e41cffa2989
Chas Fricke <chasfricke@Chass-MacBook-Pro-2.local>
2018-08-23 12:52:32 -0600

    navbar is responsive

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -16,13 +16,14 @@ class Header extends Component {
             <div>
                 <Head>
                     <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
+                    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
                     <meta
                         name="viewport"
                         content="width=device-width, initial-scale=1"
                     />
                     <meta charSet="utf-8" />
                     <title>
-                        WordPress + React Starter Kit Frontend by Postlight
+                        Vangst
                     </title>
                 </Head>
             </div>
--- frontend/components/Menu.js
+++ frontend/components/Menu.js
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 import Link from "next/link";
 import { Config } from "../config.js";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
 
 const linkStyle = {
     marginRight: 15
@@ -9,28 +21,71 @@ const linkStyle = {
 class Menu extends Component {
   constructor() {
       super();
+
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        isOpen: false
+      };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
   }
 
   render() {
     return(
       <div>
-        <div className="nav-menu">
-            <Link href="/">
-                <a style={linkStyle}>JOB SEEKERS</a>
-            </Link>
-            <Link href="/">
-                <a style={linkStyle}>EMPLOYERS</a>
-            </Link>
-            <Link href="/">
-                <a style={linkStyle}>HOW IT WORKS</a>
-            </Link>
-            <Link href="/">
-                <a style={linkStyle}>CAREER FAIR</a>
-            </Link>
-            <Link href="/">
-                <a style={linkStyle}>ABOUT</a>
-            </Link>
-        </div>
+        <Navbar color="light" light expand="lg">
+            <NavbarBrand href="/">
+                <div className="brand-container">
+                    <img className="vangst-logo-graphic d-lg-none" src="/static/images/logo-graphic.png" alt="vangst logo"></img>
+                    <img className="vangst-logo-text d-lg-none" src="/static/images/logo-text.png" alt="vangst logo text"></img>
+                    <img className="vangst-logo d-none d-lg-block" src="/static/images/logo.png" alt="vangst logo"></img>
+                </div>
+            </NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+                <Nav className="ml-auto" navbar>
+                <NavItem>
+                    <NavLink href="/">JOB SEEKERS</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/">EMPLOYERS</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/">HOW IT WORKS</NavLink>
+                </NavItem>
+                <NavItem>
+                    <NavLink href="/">CAREER FAIR</NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                    ABOUT
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                    <DropdownItem>
+                        Option 1
+                    </DropdownItem>
+                    <DropdownItem>
+                        Option 2
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                        Reset
+                    </DropdownItem>
+                    </DropdownMenu>
+                </UncontrolledDropdown>
+                <NavItem className="nav-item dropdown" id="sign-in">
+                    <a className="nav-link">SIGN IN</a>
+                </NavItem>
+                <NavItem className="nav-item dropdown bg-dark" id="sign-up">
+                    <a className="nav-link text-light">SIGN UP</a>
+                </NavItem>
+                </Nav>
+            </Collapse>
+        </Navbar>
       </div>
     )
   }
