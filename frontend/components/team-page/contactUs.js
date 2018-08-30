414750e1ed7e0432b1494345e5cf8ee10b85a35f
AustenElswick <austen.elswick@gmail.com>
2018-08-30 12:51:00 -0600

    added skeleton components for team page

---
+++ frontend/components/team-page/contactUs.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class ContactUs extends Component {
+  render() {
+    return (
+      <section>
+        <h4>headline for consultation</h4>
+        <p>a few words about consultation im assuming</p>
+        <div>
+          <input type='text' placeholder='Name' />
+          <input type='text' placeholder='Email' />
+          <input type='button' value='Contact Us' onClick='#' />
+        </div>
+      </section>
+    );
+  }
+}
+
+export default ContactUs;
---
+++ frontend/components/team-page/hero.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Hero extends Component {
+  render() {
+    return (
+      <section>
+        <img src='/static/images/dummyImage.png' />
+        <h1>Cannabis is who we are.</h1>
+      </section>
+    );
+  }
+}
+
+export default Hero;
