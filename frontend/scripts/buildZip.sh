8fde2c4ee5975b17cacfbb32d99af5f6cce28950
Zach Schulz <schulzzw@gmail.com>
2018-09-07 15:52:04 -0600

    just some stuff

---
+++ frontend/scripts/buildZip.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+npm run next:build;
+rm -rf ../../websiteZip.zip;
+zip ../websiteZip.zip ./*;
+zip ../websiteZip.zip ./component/**/*;
+zip ../websiteZip.zip ./pages/**/*;
+zip ../websiteZip.zip ./src/**/*;
+zip ../websiteZip.zip ./static/**/*;
+zip ../websiteZip.zip ./.next/**/*;
