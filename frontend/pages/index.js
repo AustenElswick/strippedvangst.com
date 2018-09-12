9ee4176a54133c414c0d9026201bb6f1856bf424
Chas Fricke <chasfricke@Chass-MacBook-Pro-2.local>
2018-09-12 16:52:35 -0600

    added robot.txt and xml sitemap

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,33 +3,14 @@ import React, { Component } from "react";
 import fetch from "isomorphic-unfetch";
 import Link from "next/link";
 import PageWrapper from "../components/PageWrapper.js";
-import VangstMainPage from './vangst-main-page'
+import VangstMainPage from "./vangst-main-page";
 import { Config } from "../config.js";
-import Router from 'next/router'
+import Router from "next/router";
 
 class Index extends Component {
-    static async getInitialProps(context) {
-        const pageRes = await fetch(
-            `${Config.apiUrl}/wp-json/postlight/v1/page?slug=welcome`
-        );
-        const page = await pageRes.json();
-        const postsRes = await fetch(
-            `${Config.apiUrl}/wp-json/wp/v2/posts?_embed`
-        );
-        const posts = await postsRes.json();
-        const pagesRes = await fetch(
-            `${Config.apiUrl}/wp-json/wp/v2/pages?_embed`
-        );
-        const pages = await pagesRes.json();
-        return { page, posts, pages };
-    }
-    
-    render() {
-      return (
-        <div>
-        </div>
-        );
-    }
+  render() {
+    return <div />;
+  }
 }
 
 export default PageWrapper(Index);
