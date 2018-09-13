6c5b93e4a661cd2cf8128e49a669adac48f7c50a
Chas Fricke <chasfricke@Chass-MacBook-Pro-2.local>
2018-09-13 12:18:19 -0600

    added .ebextensions

--- frontend/scripts/buildZip.sh
+++ frontend/scripts/buildZip.sh
@@ -1,9 +1,10 @@
 #!/bin/zsh
 npm run next:build;
-rm -rf ../../websiteZip.zip;
+rm -rf ../websiteZip.zip;
 zip ../websiteZip.zip ./*;
 zip ../websiteZip.zip ./component/**/*;
 zip ../websiteZip.zip ./pages/**/*;
 zip ../websiteZip.zip ./src/**/*;
 zip ../websiteZip.zip ./static/**/*;
 zip ../websiteZip.zip ./.next/**/*;
+zip ../websiteZip.zip ./.ebextensions/**/*;
